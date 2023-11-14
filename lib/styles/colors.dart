6ffe910e2bd8559422780d42eb39eb5ff2c8bf36
“rachelzn” <racheltjengerari@gmail.com>
2023-11-14 13:53:59 +0700

    create color constants

---
+++ lib/styles/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Colorz {
+  static const white = Color(0xFFffffff);
+  static const black = Color(0xFF171314);
+  static const pink = Color(0xFFF3C3FB);
+  static const avogreen = Color(0xFFDEE287);
+  static const darkgreen = Color(0xFF24554C);
+  static const oceanblue = Color(0XFF3683EF);
+  static const skyblue = Color(0xFFACD8FA);
+  static const orange = Color(0xFFE65E2B);
+  static const lightbeige = Color(0xFFFDF5F1);
+  static const beige = Color(0xFFF3E6E0);
+  static const darkbeige = Color(0xFFE7D4C6);
+  static const ashbrown = Color(0xFFB0A198);
+  static const beigebg = Color(0xFFECDED6);
+}
