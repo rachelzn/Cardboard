32a4770ddf74f3e0c359775062930703fee4026d
“rachelzn” <racheltjengerari@gmail.com>
2023-11-14 13:54:48 +0700

    create font constants

---
+++ lib/styles/fonts.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class Branding {
+  static final TextStyle B44 = TextStyle(
+    fontFamily: 'Roca Two Bold',
+    color: Colorz.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 44,
+  );
+
+  static final TextStyle B36 = TextStyle(
+    fontFamily: 'Roca Two Bold',
+    color: Colorz.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 36,
+  );
+
+  static final TextStyle B28 = TextStyle(
+    fontFamily: 'Roca Two Bold',
+    color: Colorz.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 28,
+  );
+}
