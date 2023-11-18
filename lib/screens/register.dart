0cfdd90fc2f1b97045f508fa32af71e327dc8d0e
“rachelzn” <racheltjengerari@gmail.com>
2023-11-18 22:43:25 +0700

    create sign up page

---
+++ lib/screens/register.dart
@@ -0,0 +1,267 @@
+import 'package:cardboard/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:cardboard/styles/colors.dart';
+
+void main() {
+  runApp(const RegisterApp());
+}
+
+class RegisterApp extends StatelessWidget {
+  const RegisterApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sign Up',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterPage(),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key});
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passwordConfirmationController =
+      TextEditingController();
+  bool _passwordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Register',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colorz.darkgreen,
+        foregroundColor: Colorz.pink,
+      ),
+      backgroundColor: Colorz.darkgreen,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: screenHeight / 2,
+              decoration: BoxDecoration(
+                color: Colorz.beigebg, // color of the bottom sheet
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: screenHeight / 3.5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // change the icon based on the password visibility
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          // password visibility state
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                    obscureText: !_passwordVisible, // Toggle
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    controller: _passwordConfirmationController,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                    obscureText: !_passwordVisible, // Toggle
+                  ),
+                ),
+                const SizedBox(height: 18.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+                    String passwordConfirmation =
+                        _passwordConfirmationController.text;
+
+                    // Perform input validation
+                    if (username.isEmpty ||
+                        password.isEmpty ||
+                        passwordConfirmation.isEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Registration Failed'),
+                          content: const Text('Please fill in all fields.'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                      return;
+                    }
+
+                    if (password != passwordConfirmation) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Registration Failed'),
+                          content: const Text('Passwords do not match.'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                      return;
+                    }
+                    // Registration logic, send registration request to your server API
+                    final response = await http.post(
+                      Uri.parse('http://127.0.0.1:8000/auth/register/'),
+                      body: {
+                        'username': username,
+                        'password1': password,
+                        'password2': passwordConfirmation,
+                      },
+                    );
+
+                    if (response.statusCode == 201) {
+                      // Registration successful
+                      String message = 'Registration successful. Please Log in';
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(SnackBar(
+                          content: Text(message),
+                        ));
+                    } else {
+                      // Registration failed
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Registration Failed'),
+                          content: Text(
+                              'Registration failed. Please try again later.'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colorz.avogreen,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(385, 50),
+                    backgroundColor: Colorz.black,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  "Already have an account?",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              LoginPage()), // Navigate to login page
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colorz.black,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
